1ceac6fcb30ee2b723b6c600e74890dfd815e34f
ParanovaFactory <berkay_krdmn@hotmail.com>
2024-10-03 15:20:33 +0300

    Add project files.

---
+++ randomStructure/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace randomStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int num = random.Next();
+            int num1 = random.Next(0,11); 
+            int num2 = random.Next(99); 
+            double num3 = random.NextDouble();
+            Console.WriteLine(num + " |-| " + num1 + " |-| " + num2 + " |-| " + num3);
+            Console.ReadLine();
+        }
+    }
+}
